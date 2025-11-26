f3005f7d23c0a782f60378403994fe86c6ed26fb
🚀Johany📟 <johany1996@hotmail.com>
2025-11-25 22:50:59 -0500

    feat: ajuste de footer

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     standalone: true,
     selector: 'app-footer',
-    template: `<div class="layout-footer">
-        Prueba realizada por: <a href="https://www.linkedin.com/in/johanycruz/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Johany Cruz</a>
-    </div>`
+    template: `<div class="layout-footer">Prueba realizada por: <a href="https://www.linkedin.com/in/johanycruz/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Heyner Aldana</a></div>`
 })
 export class AppFooter {}
